8fac9bac3b82ebc1f9ecaea9b4b59331cefebd93
Raul Ferrando <raulferrando@protonmail.com>
2026-07-13 22:48:23 +0200

    add gruvbox

--- lua/raulf/plugins/colorscheme.lua
+++ lua/raulf/plugins/colorscheme.lua
@@ -1,20 +1,29 @@
 return {
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     opts = {},
+    --     config = function()
+    --         vim.cmd("colorscheme tokyonight")
+    --     end,
+    -- },
+    -- {
+    --     "nickkadutskyi/jb.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {},
+    --     config = function()
+    --         -- require("jb").setup({transparent = true})
+    --         --vim.cmd("colorscheme jb")
+    --     end,
+    -- }
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
-        config = function()
-            vim.cmd("colorscheme tokyonight")
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = true,
+        init = function()
+            vim.o.background = "dark"
+            vim.cmd("colorscheme gruvbox")
         end,
     },
-    {
-        "nickkadutskyi/jb.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-        config = function()
-            -- require("jb").setup({transparent = true})
-            --vim.cmd("colorscheme jb")
-        end,
-    }
 }
